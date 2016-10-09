c2453d4614c85eaf9a8d2258c57a54eba3b918bd
Shiyao <liushiyao2014@gmail.com>
2016-10-09 10:44:23 -0500

    Add base urls

---
+++ crawlers/UrlSearcher.py
@@ -0,0 +1,19 @@
+import urllib
+import urllib2
+
+class UrlSearcher:
+    def __init__(self, base_url, begin_number, end_number, max_posts):
+        self.base_url = base_url
+        self.begin_number = begin_number
+        self.end_number = end_number
+        self.max_posts = max_posts
+
+    def search_valid_urls(self):
+        print 
+
+
+    def __get_main_page(self):
+
+    def __search_one_page(self):
+
+
