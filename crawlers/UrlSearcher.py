c78eb47604609a416c3d77205ee228eaf9124776
Shiyao <liushiyao2014@gmail.com>
2016-10-11 22:14:16 -0500

    Modify regex to have more strict search criteria

--- crawlers/UrlSearcher.py
+++ crawlers/UrlSearcher.py
@@ -4,39 +4,41 @@ import math
 import re
 
 class UrlSearcher:
-    def __init__(self, base_url, begin_number, end_number, max_posts):
-        self.base_url = base_url
-        self.begin_number = begin_number
-        self.end_number = end_number
+        def __init__(self, base_url, begin_number, end_number, max_posts):
+            self.base_url = base_url
+            self.begin_number = begin_number
+            self.end_number = end_number
 
-        if (max_posts < 100):
-            self.max_posts = max_posts
-        else:
-            self.max_posts = int((math.ceil(max_posts / 100) + 1) * 100)
-        try:
-            request = urllib2.Request(self.base_url)
-            response = urllib2.urlopen(request)
-            print ('UrlSearcher initialized, set the max posts to be %s' % self.max_posts)
-        except urllib2.URLError, e:
-            if hasattr(e,"reason"):
-                print u"connection error", e.reason
-                return None
+            if max_posts < 100:
+                self.max_posts = max_posts
+            else:
+                self.max_posts = int((math.ceil(max_posts / 100) + 1) * 100)
+            try:
+                request = urllib2.Request(self.base_url)
+                response = urllib2.urlopen(request)
+                print ('UrlSearcher initialized, set the max posts to be %s' % self.max_posts)
+            except urllib2.URLError, e:
+                if hasattr(e,"reason"):
+                    print u"connection error", e.reason
+                    return None
 
-    def search_valid_urls(self):
-        pn = 0
-        while (pn <= self.max_posts):
-            print ('Begin searching from post number: ' + str(pn))
-            url = self.base_url + '&pn=' + str(pn)
-            request = urllib2.Request(url)
-            response = urllib2.urlopen(request)
-            pattern = self.__build_pattern()
-            result = re.search(pattern, response.read())
-            if (result):
-                print result.group()
+        def search_valid_urls(self):
+            pn = 0
+            while pn <= self.max_posts:
+                print ('Begin searching from post number: ' + str(pn))
+                url = self.base_url + '&pn=' + str(pn)
+                request = urllib2.Request(url)
+                response = urllib2.urlopen(request)
+                pattern = self.__build_pattern()
+                result = re.findall(pattern, response.read())
 
-            pn += 50
-        print ('End search on the basic url...')
+                if result:
+                    for url in result:
+                        print url
 
-    def __build_pattern(self):
-        pattern = '<a href="/p/\d+" title=".*贴吧汉化.*>'
-        return pattern
+                pn += 50
+            print ('End search on the basic url...')
+
+        def __build_pattern(self):
+            pattern = r'<a href="/p/\d+" title=".*贴吧汉化.*逆转监督\s*\d+.*>'
+            return pattern
