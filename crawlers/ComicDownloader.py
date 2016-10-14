1b0445d141643f94b9d0827a3cf2920a499c2134
Shiyao Liu <liushiyao2014@gmail.com>
2016-10-14 16:39:42 -0500

    Add downloader

---
+++ crawlers/ComicDownloader.py
@@ -0,0 +1,11 @@
+import urllib2
+import os
+
+class ComicDownloader:
+    def __init__(self, img_list, comic_nbr, basic_path):
+        self.__img_list = img_list
+        self.__comic_nbr = comic_nbr
+        self.__basic_path = basic_path
+
+    def download(self):
+        print os.path.exists(self.__basic_path + '/' + str(self.__comic_nbr) + '话')
--- crawlers/ComicImageCrawler.py
+++ crawlers/ComicImageCrawler.py
@@ -1,5 +1,4 @@
 import urllib2
-import math
 import re
 
 class ComicImageCrawler:
