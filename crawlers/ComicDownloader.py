cb31cea97e79575f9809f30c6ad2081779fc799f
Shiyao <liushiyao2014@gmail.com>
2016-10-16 11:07:11 -0500

    Implement file creator

--- crawlers/ComicDownloader.py
+++ crawlers/ComicDownloader.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import urllib2
+import urllib
 import os
 
 class ComicDownloader:
@@ -7,6 +8,26 @@ class ComicDownloader:
         self.__img_list = img_list
         self.__comic_nbr = comic_nbr
         self.__basic_path = basic_path
+        if not os.path.exists(self.__basic_path):
+            os.mkdir(self.__basic_path)
 
     def download(self):
-        print os.path.exists(self.__basic_path + '/' + str(self.__comic_nbr) + '话')
+        path = self.__basic_path + '/' + str(self.__comic_nbr) + '话'
+        if not os.path.exists(path):
+            os.mkdir(path)
+        count = 1
+
+        for img_url in self.__img_list:
+            filename = str(count) + '.jpg'
+            print 'downloading ' + filename + '...'
+            self.__save_img(filename, img_url)
+            count += 1
+
+
+    def __save_img(self, filename, img_url):
+        response = urllib2.urlopen(img_url)
+        data = response.read()
+        f = open(filename, 'wb')
+        f.close()
+
+
